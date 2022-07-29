c47004eeeb213d774f2387753651511302fc44ac
Mike Smith <mike@vnerd.io>
2022-07-29 08:48:02 -0400

    initial commit

---
+++ Public/Get-RBLinks.ps1
@@ -0,0 +1,49 @@
+<#
+    .SYNOPSIS
+
+    .DESCRIPTION
+
+    .EXAMPLE
+
+    .PARAMETER
+    
+    .OUTPUTS
+
+    .NOTES
+
+    .LINK
+    https://developers.rebrandly.com/docs/api-custom-url-shortener
+#>
+Function Get-RBLinks{
+	[CmdletBinding()]
+	Param([Parameter(Mandatory=$false)]
+		  [string]
+		  $RBWSId = $global:RBWSId,
+          [Parameter(Mandatory=$false)]
+		  [string]
+          $RBKey = $global:RBKey)
+	
+	begin{
+        $Header = @{
+            "Content-Type"  = "application/json"
+            "apikey"        = $RBKey
+            "workspace"     = $RBWSId
+        }
+        $apiurl = 'https://api.rebrandly.com/v1/links'
+    }
+	process{
+		try{
+			$r = Invoke-WebRequest -Uri $apiurl -Headers $Header
+            $r.content | ConvertFrom-Json
+		}
+		catch{
+			$ErrorMessage = $_.Exception.Message
+			$FailedItem = $_.Exception.ItemName
+			
+			Write-Output "$FailedItem - $ErrorMessage"
+			Return $False
+		}
+		finally{}
+	}
+	end{}
+}
---
+++ Public/New-RBLink.ps1
@@ -0,0 +1,82 @@
+<#
+    .SYNOPSIS
+
+    .DESCRIPTION
+
+    .EXAMPLE
+
+    .PARAMETER
+    
+    .OUTPUTS
+
+    .NOTES
+
+    .LINK
+    https://developers.rebrandly.com/docs/api-custom-url-shortener
+#>
+Function New-RBLink{
+	[CmdletBinding()]
+	Param([Parameter(Mandatory=$false)]
+		  [string]
+		  $RBWSId = $global:RBWSId,
+          [Parameter(Mandatory=$false)]
+		  [string]
+          $RBKey = $global:RBKey,
+          [Parameter(Mandatory=$true)]
+		  [string]
+          $Destination,
+          [Parameter(Mandatory=$false)]
+		  [string]
+          $Slashtag,
+          [Parameter(Mandatory=$false)]
+		  [string]
+          $Title,
+          [Parameter(Mandatory=$false)]
+		  [string]
+          $Domain)
+	
+	begin{
+        $Header = @{
+            "Accept"        = "application/json"
+            "Content-Type"  = "application/json"
+            "apikey"        = $RBKey
+            "workspace"     = $RBWSId
+        }
+        $apiurl = 'https://api.rebrandly.com/v1/links'
+
+        $body = @{"destination"="$Destination"}
+        if($Domain){
+            $body += @{'domain'=@{'fullName'=$Domain}}
+        }
+        if($Slashtag){
+            $body.add("slashtag","$Slashtag")
+        }
+        if($Title){
+            $body.add("title","$Title")
+        }
+        Write-Verbose ($body | ConvertTo-Json)
+    }
+	process{
+		try{
+			$r = Invoke-WebRequest -Method Post -Uri $apiurl -Headers $Header -Body ($body | ConvertTo-Json)
+            Write-Verbose $r
+
+            if($r.StatusCode -eq "200" -AND $r.StatusDescription -eq "OK"){
+                $r | ConvertFrom-Json
+                return $true
+            } else {
+                $r
+                return $false
+            }
+		}
+		catch{
+			$ErrorMessage = $_.Exception.Message
+			$FailedItem = $_.Exception.ItemName
+			
+			Write-Output "$FailedItem - $ErrorMessage : $r"
+			Return $False
+		}
+		finally{}
+	}
+	end{}
+}
